4686d01315a6f3595d838ac679c7198092412623
Rumen B. Balev <r.balev@gmail.com>
2023-12-16 16:42:41 +0200

    Fixing some missed URL in tests

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/shopping-List');
+    await page.goto('https://rumenbalev-exam.onrender.com/shopping-List');
     const list = await page.$('a[href="/Shopping-List"]');
     expect(list).toBeTruthy();
   });
